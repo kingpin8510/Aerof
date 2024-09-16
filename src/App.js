2e8a2a620d702dddae9f91fe65275d8874c2c8de
kingpin8510 <rp240479@gmail.com>
2024-09-17 03:21:31 +0530

    App.js update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Login from "./components/Login";
 import Home from "./container/Home";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { fetchUser } from "./utils/fetchUser";
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if(!user) navigate('/login');
+  }, [])
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
